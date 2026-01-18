7cbacb9a566378a4341f528b4bb6441a598ec80d
Sanfertour <albertoortizmijoler@gmail.com>
2026-01-18 14:56:54 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -21,19 +21,18 @@ const App = () => {
           
           {/* FONDO FIJO: La atmósfera que NO se mueve al hacer scroll */}
           <div className="fixed inset-0 pointer-events-none z-0 overflow-hidden">
-            {/* Onda Sky Blue - Calma para la Tribu */}
-            <div className="absolute -top-[10%] -left-[10%] w-[700px] h-[700px] bg-sky-400/20 blur-[100px] animate-wave-slow" />
+            {/* Onda Sky Blue - Calma para la Tribu: Más marcada y menos blur */}
+            <div className="absolute -top-[15%] -left-[10%] w-[800px] h-[800px] bg-sky-400/40 blur-[60px] animate-wave-slow" />
             
-            {/* Onda Vital Orange - Energía para el Equipo */}
-            <div className="absolute -bottom-[10%] -right-[10%] w-[600px] h-[600px] bg-orange-400/20 blur-[100px] animate-wave-medium" />
+            {/* Onda Vital Orange - Energía para el Equipo: Opacidad subida al 40% */}
+            <div className="absolute -bottom-[15%] -right-[10%] w-[700px] h-[700px] bg-orange-400/40 blur-[60px] animate-wave-medium" />
             
-            {/* Punto de luz violeta para profundidad visual */}
-            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[500px] h-[500px] bg-violet-300/10 blur-[120px] animate-float" />
+            {/* Punto de luz violeta: Reducimos blur para dar más textura */}
+            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[500px] h-[500px] bg-violet-400/15 blur-[80px] animate-float" />
           </div>
 
-          {/* CONTENIDO FLUIDO: La capa que permite el scroll natural de la agenda y tribu */}
+          {/* CONTENIDO FLUIDO: La capa que permite el scroll natural */}
           <div className="relative z-10 w-full"> 
-            {/* Eliminamos cualquier restricción de altura para que el scroll sea infinito */}
             <BrowserRouter>
               <Routes>
                 <Route path="/" element={<Index />} />
